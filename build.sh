3468249c6a449c57a3d8b8418b88716936f044f4
Laudivan <laudivan@users.noreply.github.com>
2026-08-20 04:11:24 +0000

    feat(build): refine build script for bundle generation and manifest templating

--- build.sh
+++ build.sh
@@ -9,63 +9,79 @@ BUILD="6512087774658560"
 
 PROJECT_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
 
-BUILD_DIR="/tmp/antigravity_flatpak"
+BUILD_DIR="/tmp/unofficial-antigravity.flatpak"
 
 BASE_URL="https://storage.googleapis.com/antigravity-public/antigravity-hub"
 
 FILE="Antigravity.tar.gz"
 
-[ -d "${BUILD_DIR}" ] && sudo rm -fr "${BUILD_DIR}"
+rm -fr repo
 
-[ -d /tmp/repo ] && sudo rm -fr /tmp/repo
+[ -d "$BUILD_DIR" ] && sudo rm -fr "$BUILD_DIR"
 
-mkdir -p "${BUILD_DIR}" /tmp/repo
+# mkdir -p "${BUILD_DIR}"
 
-cp -r "${PROJECT_DIR}"/* "${BUILD_DIR}/"
+cp -dpr "$PROJECT_DIR" "$BUILD_DIR"
 
-cd "${BUILD_DIR}"
+# magick -density 90 -background none "$APP_ID.svg" "$APP_ID.png"
 
-magick -density 90 -background none "${APP_ID}.svg" "${APP_ID}.png"
+DT="$(date +%Y-%m-%d)"
 
-sed "s/{version}/${APP_VERSION}/" "${APP_ID}.metainfo.xml"
+sed -i \
+    -e "s/{version}/$APP_VERSION/g" \
+    -e "s/{build}/$BUILD/g" \
+    -e "s/{date}/$DT/g" \
+    "$BUILD_DIR/$APP_ID.metainfo.xml"
 
-sed "s/{build}/${BUILD}/" "${APP_ID}.metainfo.xml"
-
-sed "s/{build}/$(date +%Y-%m-%d)/" "${APP_ID}.metainfo.xml"
+sed -i \
+    -e "s/{version}/$APP_VERSION/g" \
+    -e "s/{build}/$BUILD/g" \
+    "$BUILD_DIR/$APP_ID.yaml"
 
 for ARCH in {arm,x64}; do
-    URL="${BASE_URL}/${APP_VERSION}-${BUILD}/linux-${ARCH}/${FILE}"
+    URL="$BASE_URL/$APP_VERSION-$BUILD/linux-$ARCH/$FILE"
 
-    curl -sSL -o "${FILE}" "${URI}"
+    curl -sSL -o "$BUILD_DIR/$FILE" "$URL"
 
-    SIZE="$(stat -c %s "${FILE}")"
+    SIZE="$(stat -c %s "$BUILD_DIR/$FILE")"
 
-    SHA256="$(sha256sum "${FILE}" | cut -d ' ' -f1)"
+    SHA="$(sha256sum "$BUILD_DIR/$FILE" | cut -d ' ' -f1)"
 
-    sed "s/<url-${ARCH}>/${URI}/" "${APP_ID}.yaml"
+    sed -i \
+        -e "s/{size_$ARCH}/$SIZE/" \
+        -e "s/{sha_$ARCH}/$SHA/" \
+        "$BUILD_DIR/$APP_ID.yaml"
 
-    sed "s/<size-${ARCH}>/${SIZE}/" "${APP_ID}.yaml"
-
-    sed "s/<sha-${ARCH}>/${SHA256}/" "${APP_ID}.yaml"
-
-    rm -f "${FILE}"
+    rm -f "$BUILD_DIR/$FILE"
 done
 
-sudo \
+mkdir -p "$BUILD_DIR/repo"
+
+sudo sh <<SCRIPT
+    cd "$BUILD_DIR"
+
     flatpak-builder --install-deps-from=flathub \
-	    --repo=/tmp/repo --force-clean \
-	    build "${APP_ID}.yaml" && \
+	    --repo=repo --force-clean \
+	    build "$APP_ID.yaml"
+
     flatpak build-bundle \
-	    --runtime-repo=https://dl.flathub.org/repo/flathub.flatpakrepo \
-	    /tmp/repo \
-	    "/tmp/repo/${APP_ID}.flatpak" \
-	    "${APP_ID}" && \
-	mv /tmp/repo "${PROJECT_DIR}" && \
-	chown $USER:$USER -R "${PROJECT_DIR}/repo" && \
-	rm -fr "${BUILD_DIR}"
+	    repo \
+	    "$APP_ID.flatpak" \
+	    "$APP_ID"
 
-flatpak --user install --assumeyes "${PROJECT_DIR}/repo" "${APP_ID}"
+	chown $USER:$USER -R "$APP_ID.flatpak"
 
-flatpak --user run \
-    --verbose --devel --log-a11y-bus --log-session-bus --log-system-bus \
-    "${APP_ID}"
+	[ -f "$PROJECT_DIR/$APP_ID.flatpak" ] && rm -f "$PROJECT_DIR/$APP_ID.flatpak"
+
+	cp -dpf "$BUILD_DIR/$APP_ID.flatpak" "$PROJECT_DIR/$APP_ID.flatpak"
+
+	cd /
+
+	# rm -fr "$BUILD_DIR"
+SCRIPT
+
+# flatpak --user install --assumeyes "${APP_ID}.flatpak"
+
+# flatpak --user run \
+#    --verbose --devel --log-a11y-bus --log-session-bus --log-system-bus \
+#    "${APP_ID}"
